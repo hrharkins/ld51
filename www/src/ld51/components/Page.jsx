eb43a9210490c9b1fd9998d94101899f1a533fd6
Rich Harkins <rich.harkins@gmail.com>
2022-09-30 21:49:22 +0000

    More prep

--- www/src/ld51/components/Page.jsx
+++ www/src/ld51/components/Page.jsx
@@ -3,21 +3,42 @@ import {
   AppBar,
   Box,
   Container,
+  createTheme,
+  Link,
+  ThemeProvider,
   Toolbar,
   Typography,
 } from '@mui/material';
+import {
+    GitHub
+} from '@mui/icons-material';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function Page(props)
 {
   const title = props.title;
   return (
+    <ThemeProvider theme={ darkTheme }>
     <Box>
       <AppBar>
-        <Toolbar>
-          <Typography variant='h6' component='div'>
-            {title}
-          </Typography>
-        </Toolbar>
+        <Container>
+          <Toolbar>
+            <Link href="https://github.com/hrharkins/ld51" 
+                  target="https://github.com/hrharkins/ld51" 
+                  sx={{ size: "large" }} mr
+                  color="inherit">
+              <GitHub/>
+            </Link>
+            <Typography variant='h6' component='div'>
+              {title}
+            </Typography>
+          </Toolbar>
+        </Container>
       </AppBar>
       <Toolbar/>
       <Container>
@@ -26,5 +47,6 @@ export default function Page(props)
         </Typography>
       </Container>
     </Box>      
+    </ThemeProvider>
   );    
 }
